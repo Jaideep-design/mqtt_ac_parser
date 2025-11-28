d807b08f58daabb17f5d51a704be33abca97217f
Jaideep-design <jaideep.gupta@ecozensolutions.com>
2025-11-28 14:51:32 +0530

    Update Mannual_parser.py

--- streamlit_app/pages/Mannual_parser.py
+++ streamlit_app/pages/Mannual_parser.py
@@ -1,3 +1,6 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+
 import pandas as pd
 import streamlit as st
 from dictionary_utils import excel_to_json
